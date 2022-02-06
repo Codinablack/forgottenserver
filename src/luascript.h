35c4c97d7469b979f182904522a55bcf817c20c9
Erza <exposedmail@protonmail.ch>
2022-02-06 02:24:02 +0100

    Add Player:getIdleTime and Player:isNearDepotBox Lua functions (#3912)
    (cherry picked from commit 60fe34939901c12ad22773b7e405e1be78490c9e)

--- src/luascript.h
+++ src/luascript.h
@@ -1044,6 +1044,10 @@ class LuaScriptInterface
 
 		static int luaPlayerGetStoreInbox(lua_State* L);
 
+		static int luaPlayerIsNearDepotBox(lua_State* L);
+
+		static int luaPlayerGetIdleTime(lua_State* L);
+
 		// Monster
 		static int luaMonsterCreate(lua_State* L);
 
