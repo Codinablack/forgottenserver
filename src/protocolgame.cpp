170f2b66a08644b90bba69136a1702db850effe0
Ranieri Althoff <1993083+ranisalt@users.noreply.github.com>
2022-04-22 22:09:33 +0000

    Fix checksum mode for new Qt client ids (#4088)

--- src/protocolgame.cpp
+++ src/protocolgame.cpp
@@ -328,8 +328,18 @@ void ProtocolGame::onRecvFirstMessage(NetworkMessage& msg)
 		opcodeMessage.add<uint16_t>(0x00);
 		writeToOutputBuffer(opcodeMessage);
 	}
+	// Change packet verifying mode for QT clients
+	if (version >= 1111 && operatingSystem >= CLIENTOS_QT_LINUX && operatingSystem < CLIENTOS_OTCLIENT_LINUX) {
+		setChecksumMode(CHECKSUM_SEQUENCE);
+	}
 
-	msg.skipBytes(1); // gamemaster flag
+	// Web login skips the character list request so we need to check the client version again
+	if (version < CLIENT_VERSION_MIN || version > CLIENT_VERSION_MAX) {
+		disconnectClient(fmt::format("Only clients with protocol {:s} allowed!", CLIENT_VERSION_STR));
+		return;
+	}
+
+	msg.skipBytes(1); // Gamemaster flag
 
 	std::string sessionKey = msg.getString();
 
